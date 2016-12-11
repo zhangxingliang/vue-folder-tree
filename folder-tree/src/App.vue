573823de80f9816e011fee05bfff4bd0caf38f56
unknown <>
2016-12-12 00:40:19 +0800

    first commit

---
+++ folder-tree/src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <tree :data="node" v-for="node in nodes"></tree>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Tree from './components/tree_ctrl.vue'
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      nodes: [
+        {
+          name: 'MaterialList',
+          selected: false,
+          selecting: false,
+          open: true,
+          path: '/MaterialList',
+          floor: 1,
+          type: 'folder',
+          children: [
+            {
+              name: 'MaterialList',
+              selected: false,
+              selecting: false,
+              open: true,
+              path: '/MaterialList',
+              floor: 2,
+              type: 'folder',
+              children: []
+            },
+            {
+              name: 'Search Result',
+              selected: false,
+              selecting: false,
+              open: true,
+              path: '/Search Result',
+              floor: 2,
+              type: 'folder',
+              children: []
+            }
+          ]
+        },
+        {
+          name: 'Search Result',
+          selected: false,
+          selecting: false,
+          open: true,
+          path: '/Search Result',
+          floor: 1,
+          type: 'folder',
+          children: [
+            {
+              name: 'MaterialList',
+              selected: false,
+              selecting: false,
+              open: true,
+              path: '/MaterialList',
+              floor: 2,
+              type: 'folder',
+              children: []
+            },
+            {
+              name: 'Search Result',
+              selected: false,
+              selecting: false,
+              open: true,
+              path: '/Search Result',
+              floor: 2,
+              type: 'folder',
+              children: [
+                {
+                  name: 'MaterialList',
+                  selected: false,
+                  selecting: false,
+                  open: true,
+                  path: '/MaterialList',
+                  floor: 3,
+                  type: 'folder',
+                  children: []
+                },
+                {
+                  name: 'Search Result',
+                  selected: false,
+                  selecting: false,
+                  open: true,
+                  path: '/Search Result',
+                  floor: 3,
+                  type: 'folder',
+                  children: []
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  },
+  components: {
+    Hello,
+    Tree
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ folder-tree/src/components/tree_ctrl.vue
@@ -0,0 +1,71 @@
+<template>
+  <li>
+    <div :title="node.name" class="folder_container" :class="{folder_selected : node.selected}" @click="click" @dblclick="dblclick" :style="{paddingLeft:20 * node.floor + 'px'}">
+      <span class="folder_icon" :class="node.open ? 'icon_openned' : 'icon_closed'" @click="toggle"></span>
+      <span :class="node.icon" class="folder_icon"> </span>
+      <span class="folder_name">{{node.name}}</span>
+    </div>
+    <ul v-show="node.open">
+      <tree-ctrl :data="n" v-for="n in node.children"></tree-ctrl2>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+    data: Object
+  },
+  name: 'tree-ctrl',
+  methods: {
+    click: function () {
+    },
+    dblclick: function () {
+    },
+    toggle: function () {
+    }
+  },
+  computed: {
+    node () {
+      return this.data
+    }
+  }
+}
+</script>
+<style scoped>
+ul,ol,li
+{
+  display: block;
+  list-style-type: none;
+  -webkit-margin-before: 0px;
+  -webkit-margin-after: 0px;
+  -webkit-margin-start: 0px;
+  -webkit-margin-end: 0px;
+  -webkit-padding-start: 0px;
+  padding: 0;
+  margin: 0;
+}
+.folder_container ul
+{
+  padding-left: 10px;
+}
+.folder_container
+{
+	background-color: #222;
+  cursor: default;
+  font-weight: normal;
+  height: 25px;    line-height: 25px;
+	font-size:12px;
+	color:#cfd2d4;
+	padding-left:10px;
+}
+.folder_container:hover
+{
+	background-color:#f5a623;
+	color:#fff;
+}
+.folder_selected
+{
+	background-color:#f5a623;
+	color:#000;
+}
+</style>
